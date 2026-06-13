48c5d9e5cab0b0cb8cdc5d188b4988b861bb650c
Md-Rashid-Imran <rashidimran78900987@gmail.com>
2026-06-13 13:10:48 +0530

    Initial setup: Spring Boot with Thymeleaf and MySQL

---
+++ src/main/java/com/rental/property_system/PropertySystemApplication.java
@@ -0,0 +1,13 @@
+package com.rental.property_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rental/property_system/PropertySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.property_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
